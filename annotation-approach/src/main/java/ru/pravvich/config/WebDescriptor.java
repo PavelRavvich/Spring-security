27328c7a0566ae98ead89582a59e4979cda24bca
Pavel <triodjangopiter@yandex.ru>
2017-08-15 14:02:52 +0300

    add web descriptor

--- annotation-approach/src/main/java/ru/pravvich/config/WebDescriptor.java
+++ annotation-approach/src/main/java/ru/pravvich/config/WebDescriptor.java
@@ -1,12 +1,26 @@
 package ru.pravvich.config;
 
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
 /**
  * Author : Pavel Ravvich.
  * Created : 14.08.17.
- * <p>
- * WebDescriptor
  */
-public class WebDescriptor {
+public class WebDescriptor extends AbstractAnnotationConfigDispatcherServletInitializer {
 
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SpringRootConfig.class};
+    }
 
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringWebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"*.do"};
+    }
 }
