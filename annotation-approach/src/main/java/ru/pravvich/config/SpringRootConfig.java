171b4a07adc12db4100e9ec9643712b77eeb4326
Pavel <triodjangopiter@yandex.ru>
2017-08-15 14:01:46 +0300

    add root config

--- annotation-approach/src/main/java/ru/pravvich/config/SpringRootConfig.java
+++ annotation-approach/src/main/java/ru/pravvich/config/SpringRootConfig.java
@@ -1,12 +1,13 @@
 package ru.pravvich.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Author : Pavel Ravvich.
  * Created : 14.08.17.
- * <p>
- * SpringRootConfig
  */
+@Configuration
+@ComponentScan("ru.pravvich")
 public class SpringRootConfig {
-
-
 }
