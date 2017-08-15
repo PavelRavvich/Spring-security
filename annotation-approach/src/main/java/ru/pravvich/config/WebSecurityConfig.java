68a33ecd50518848824f76406580d01f2e231e4f
Pavel <triodjangopiter@yandex.ru>
2017-08-15 14:03:38 +0300

    add WebSecurity config

--- annotation-approach/src/main/java/ru/pravvich/config/WebSecurityConfig.java
+++ annotation-approach/src/main/java/ru/pravvich/config/WebSecurityConfig.java
@@ -1,12 +1,37 @@
 package ru.pravvich.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 /**
  * Author : Pavel Ravvich.
  * Created : 14.08.17.
- * <p>
- * WebSecurityConfig
  */
-public class WebSecurityConfig {
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 
+        auth.inMemoryAuthentication()
+                .withUser("1").password("1").roles("user");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login.do")
+                .successForwardUrl("/successes_auth.do")
+                .permitAll();
+    }
 }
