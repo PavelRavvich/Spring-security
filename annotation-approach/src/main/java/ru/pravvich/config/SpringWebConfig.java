59341be8a228fd86a998bd885ea146c2a5057f9d
Pavel <triodjangopiter@yandex.ru>
2017-08-15 14:02:16 +0300

    add web config

--- annotation-approach/src/main/java/ru/pravvich/config/SpringWebConfig.java
+++ annotation-approach/src/main/java/ru/pravvich/config/SpringWebConfig.java
@@ -1,12 +1,46 @@
 package ru.pravvich.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
 /**
  * Author : Pavel Ravvich.
  * Created : 14.08.17.
- * <p>
- * SpringWebConfig
  */
-public class SpringWebConfig {
+@EnableWebMvc
+@Configuration
+@ComponentScan("ru.pravvich")
+public class SpringWebConfig extends WebMvcConfigurerAdapter {
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        /* This resources relation webapp. Its NOT java/resources !!! */
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
 
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver =
+                new InternalResourceViewResolver();
+
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
 }
