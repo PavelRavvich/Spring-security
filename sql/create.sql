785ef1d5734a6111ccd3d7ac32fadbe6df4f599e
Pavel <triodjangopiter@yandex.ru>
2017-08-15 22:35:01 +0300

    change sql schema

--- sql/create.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE spring_security;
-
-CREATE TABLE IF NOT EXISTS users (
-  id       SERIAL PRIMARY KEY,
-  username VARCHAR(20),
-  password VARCHAR(20),
-  role     VARCHAR(5)
-);
---
+++ xml-approach/sql/create.sql
@@ -0,0 +1,24 @@
+--CREATE DATABASE spring_security;
+
+CREATE TABLE users (
+  id       SERIAL      NOT NULL,
+  username VARCHAR(20) NOT NULL,
+  password VARCHAR(20) NOT NULL,
+  enabled  BOOLEAN     NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (username)
+);
+
+CREATE TABLE user_roles (
+  user_role_id SERIAL PRIMARY KEY,
+  username     VARCHAR(20) NOT NULL,
+  role         VARCHAR(20) NOT NULL,
+  UNIQUE (username, role),
+  FOREIGN KEY (username) REFERENCES users (username)
+);
+
+INSERT INTO users (id, username, password, enabled) VALUES (DEFAULT ,'jack', 'jack', TRUE);
+INSERT INTO users (id, username, password, enabled) VALUES (DEFAULT ,'peter', 'peter', TRUE);
+
+INSERT INTO user_roles (username, role) VALUES ('jack', 'ROLE_USER');
+INSERT INTO user_roles (username, role) VALUES ('jack', 'ROLE_ADMIN');
+INSERT INTO user_roles (username, role) VALUES ('peter', 'ROLE_USER');
