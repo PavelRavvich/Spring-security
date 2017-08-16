b17ed741131aa9323ae564c9ce50cff07ad61882
Pavel <triodjangopiter@yandex.ru>
2017-08-16 23:16:26 +0300

    add User repo test

--- xml-approach/src/test/java/ru/pravvich/repository/UserRepositoryTest.java
+++ xml-approach/src/test/java/ru/pravvich/repository/UserRepositoryTest.java
@@ -2,21 +2,45 @@ package ru.pravvich.repository;
 
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.pravvich.model.User;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-public class RepositoryTest {
+public class UserRepositoryTest {
 
     private final ClassPathXmlApplicationContext context =
             new ClassPathXmlApplicationContext("spring-data-context.xml");
 
-    private 
+    private UserRepository repository = context.getBean(UserRepository.class);
 
+    /**
+     * Find User by username & password.
+     *
+     * WARNING!!!
+     *
+     * Database should contain:
+     *
+     * INSERT INTO users (id, username, password, enabled) VALUES (DEFAULT ,'peter', 'peter', TRUE);
+     * INSERT INTO user_roles (username, role) VALUES ('peter', 'ROLE_USER');
+     */
     @Test
     public void whenUserExistInDBThenGetUser() {
 
+        final User result = repository
+                .findDistinctFirstByUsernameAndPassword("peter", "peter");
 
-
+        assertNotNull(result);
+        assertThat(result.getUsername(), is("peter"));
+        assertThat(result.getPassword(), is("peter"));
     }
 
+    @Test
+    public void whenGetUserWhichNotExistThenGetNull() {
+
+        final User result = repository
+                .findDistinctFirstByUsernameAndPassword("~?>", "*=/*");
+
+        assertNull(result);
+    }
 }
