1fa9557752b14677be3971f16e745d2f6821f80a
Pavel <triodjangopiter@yandex.ru>
2017-08-16 23:14:49 +0300

    add Role repo

--- xml-approach/src/main/java/ru/pravvich/repository/RoleRepository.java
+++ xml-approach/src/main/java/ru/pravvich/repository/RoleRepository.java
@@ -1,12 +1,14 @@
 package ru.pravvich.repository;
 
+import org.springframework.data.repository.CrudRepository;
+import ru.pravvich.model.Role;
+
+import java.util.List;
+
 /**
  * Author : Pavel Ravvich.
  * Created : 16.08.17.
- * <p>
- * RoleRepository
  */
-public class RoleRepository {
-
-
+public interface RoleRepository extends CrudRepository<Role, Integer> {
+    List<Role> findByUsername(String username);
 }
