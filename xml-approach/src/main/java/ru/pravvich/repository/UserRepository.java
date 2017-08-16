b20ddbc22ddbed32bbd510126dde483b4a4adf52
Pavel <triodjangopiter@yandex.ru>
2017-08-16 23:15:16 +0300

    add User repo

--- xml-approach/src/main/java/ru/pravvich/repository/UserRepository.java
+++ xml-approach/src/main/java/ru/pravvich/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package ru.pravvich.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import ru.pravvich.model.User;
 
-public interface Repository extends CrudRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Integer> {
+    User findDistinctFirstByUsernameAndPassword(String username, String password);
 }
+
